c255ae1c69fcfdf262ad9c829677a444a5f232b8
wodpachua <molarag21996@gmail.com>
2024-02-29 01:09:04 +0300

    Create function to get user choice and play one round of the game

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
-    randomChoice = Math.floor(Math.random()*3);
-    return compChoice = choices[randomChoice];
+  const choices = ["Rock", "Paper", "Scissors"];
+  randomChoice = Math.floor(Math.random() * 3);
+  return choices[randomChoice];
+}
+
+const compChoice = getComputerChoice();
+console.log('Computer Choice is:' + compChoice);
+const playerChoice = prompt('"Rock", "Paper" or "Scissors"? Enter your choice');
+
+function playOneRound(playerChoice, compChoice) {
+  if (playerChoice == compChoice) {
+    console.log(`Ta-da! You Win! ${playerChoice} beats ${compChoice}`);
+  } else {
+    console.log(`Oops! You Loose! ${compChoice} beats ${playerChoice}`);
+  }
 };
 
-console.log(getComputerChoice());
+console.log(playOneRound(playerChoice, compChoice));
