a32dc0e7c3f7d8fa2294e37d135fe44f42680d91
wodpachua <molarag21996@gmail.com>
2024-02-29 13:33:30 +0300

    Update single round function

--- script.js
+++ script.js
@@ -5,15 +5,43 @@ function getComputerChoice() {
 }
 
 const compChoice = getComputerChoice();
-console.log('Computer Choice is:' + compChoice);
+console.log("Computer Choice is:" + compChoice);
 const playerChoice = prompt('"Rock", "Paper" or "Scissors"? Enter your choice');
 
+var compWins = 0;
+var playerWins = 0;
+var roundCount = 0;
+
 function playOneRound(playerChoice, compChoice) {
-  if (playerChoice == compChoice) {
-    console.log(`Ta-da! You Win! ${playerChoice} beats ${compChoice}`);
+  roundCount++;
+
+  if (
+    (playerChoice.toLowerCase() == "rock" &&
+      compChoice.toLowerCase() == "scissors") ||
+    (playerChoice.toLowerCase() == "paper" &&
+      compChoice.toLowerCase() == "rock") ||
+    (playerChoice.toLowerCase() == "scissors" &&
+      compChoice.toLowerCase() == "paper")
+  ) {
+    playerWins++;
+    console.log(`Ta-daa! Its a Win! ${playerChoice} beats ${compChoice}`);
+    return "Win";
+  } else if (playerChoice.toLowerCase() == compChoice.toLowerCase()) {
+    console.log(`What a Tie! Its ${playerChoice} and ${compChoice}`);
+    return "Tie";
   } else {
+    compWins++;
     console.log(`Oops! You Loose! ${compChoice} beats ${playerChoice}`);
+    return "Lose";
   }
 };
 
+
+function playGame() {
+  let roundCount = 0
+  for (roundCount ; roundCount < 5; roundCount++) {}
+
+};
+
 console.log(playOneRound(playerChoice, compChoice));
+console.log(`Your Wins: ${playerWins} \nComputer Wins: ${compWins} \nRounds: ${roundCount}`);
